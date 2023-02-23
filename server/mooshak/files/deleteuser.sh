e7323a919437f1067a761a9d7f4228fbebf09198
sepgh <sepehrgh01@gmail.com>
2023-02-23 12:52:52 +0330

    feat: improved ownership of important shell scripts

--- server/mooshak/files/deleteuser.sh
+++ server/mooshak/files/deleteuser.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo deluser --remove-home $1
-ps auxwww | grep sshd: | grep $1 | grep root | awk '{ print $1 }' | while read line; do pkill -s "$line"; done
+sudo ps auxwww | grep sshd: | grep $1 | grep root | awk '{ print $1 }' | while read line; do pkill -s "$line"; done
